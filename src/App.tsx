b8949c0401bd1991117a7dcb7c5150cf54d81922
caophuoclong <caophuoclong1@gmail.com>
2021-11-22 16:24:08 +0700

    Init electron with react ts

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="">
+      <h1 className="text-3xl">Hello my best friend</h1>
+      </div>
+  );
+};
+
+export default App;
